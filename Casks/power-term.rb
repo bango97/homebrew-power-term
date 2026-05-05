d8807769935df453911fb29ee26f94a691ec22c4
band <ndba97@gmail.com>
2026-05-05 09:16:44 +0700

    feat: bump power-term to 0.1.8

--- Casks/power-term.rb
+++ Casks/power-term.rb
@@ -1,9 +1,9 @@
 cask "power-term" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.1.7"
-  sha256 arm:   "a2d38f93aa8de6e578675b69ee762e4e5e27c12b1395a0bfe1957515c7668987",
-         intel: "3e0784164728fada8b1f6d0fff587e256c06618ec760661f21b596acf66670d4"
+  version "0.1.8"
+  sha256 arm:   "1b892deed676dc41f34828f5d3dc8d0a18d8abacb588146cbbdac17382e10473",
+         intel: "77a44c53e4f53252d704a3244e828116f15a6f4ba458b18b81d8ed9adc30b343"
 
   url "https://github.com/bango97/homebrew-power-term/releases/download/v#{version}/PowerTerm-#{version}-#{arch}.dmg"
   name "Power Term"
